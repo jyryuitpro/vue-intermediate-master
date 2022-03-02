f1dbfedbcd537bdd5c000d111b231d05a11b8a20
jyryuitpro <jy.ryu.itpro@gmail.com>
2022-03-03 06:38:49 +0900

    TodoHeader 컴포넌트 구현

--- vue-todo/src/components/TodoHeader.vue
+++ vue-todo/src/components/TodoHeader.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
-    header
-  </div>
+  <header>
+    <h1>TODO it!</h1>
+  </header>
 </template>
 
 <script>
@@ -11,5 +11,9 @@ export default {
 </script>
 
 <style scoped>
-
+h1 {
+  color: #2F3B52;
+  font-weight: 900;
+  margin: 2.5rem 0 1.5rem;
+}
 </style>
