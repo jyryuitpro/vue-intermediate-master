ac8e6fdcafffd2873b2909122923489355d3234c
jyryuitpro <jy.ryu.itpro@gmail.com>
2022-03-07 22:02:56 +0900

    헬퍼 함수가 주는 간편함

---
+++ vue-todo/src/components/Demo.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="root">
+    <p>{{ this.$store.getters.originalPrice }}</p>
+    <p>{{ this.$store.getters.doublePrice }}</p>
+    <p>{{ this.$store.getters.triplePrice }}</p>
+    <p>{{ originalPrice }}</p>
+    <p>{{ doublePrice }}</p>
+    <p>{{ triplePrice }}</p>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex"
+
+export default {
+  name: "Demo",
+  computed: {
+    ...mapGetters(['originalPrice','doublePrice','triplePrice']),
+    // originalPrice() {
+    //   return this.$store.getters.originalPrice;
+    // },
+    // doublePrice() {
+    //   return this.$store.getters.doublePrice;
+    // },
+    // triplePrice() {
+    //   return this.$store.getters.triplePrice;
+    // }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
