644b96ad50677d46d6923d72d1a255312e8a5b3c
jyryuitpro <jy.ryu.itpro@gmail.com>
2022-03-07 11:23:28 +0900

    [퀴즈] 모달 컴포넌트의 slot 소개 및 퀴즈 안내

--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -5,18 +5,18 @@
       <i class="fas fa-plus addBtn"></i>
     </span>
     <!-- use the modal component, pass in the prop -->
-    <Modal v-if="showModal" @close="showModal = false">
+    <ModalAlert v-if="showModal" @close="showModal = false">
       <!--
         you can use custom content here to overwrite
         default content
       -->
-      <h3 slot="header">custom header</h3>
-    </Modal>
+      <h3 slot="header">경고!</h3>
+    </ModalAlert>
   </div>
 </template>
 
 <script>
-import Modal from './common/Modal.vue'
+import ModalAlert from './common/ModalAlert.vue'
 
 export default {
   name: "TodoInput",
@@ -33,7 +33,7 @@ export default {
         this.$emit('addTodoItem', this.newTodoItem);
         this.clearInput();
       } else {
-
+        this.showModal = !this.showModal;
       }
     },
     clearInput: function () {
@@ -41,7 +41,7 @@ export default {
     }
   },
   components: {
-    'Modal': Modal
+    'ModalAlert': ModalAlert
   }
 }
 </script>
--- vue-todo/src/components/common/ModalAlert.vue
+++ vue-todo/src/components/common/ModalAlert.vue
@@ -32,7 +32,7 @@
 
 <script>
 export default {
-  name: "Modal"
+  name: "ModalAlert"
 }
 </script>
 
@@ -48,12 +48,10 @@ export default {
   display: table;
   transition: opacity .3s ease;
 }
-
 .modal-wrapper {
   display: table-cell;
   vertical-align: middle;
 }
-
 .modal-container {
   width: 300px;
   margin: 0px auto;
@@ -64,20 +62,16 @@ export default {
   transition: all .3s ease;
   font-family: Helvetica, Arial, sans-serif;
 }
-
 .modal-header h3 {
   margin-top: 0;
   color: #42b983;
 }
-
 .modal-body {
   margin: 20px 0;
 }
-
 .modal-default-button {
   float: right;
 }
-
 /*
  * The following styles are auto-applied to elements with
  * transition="modal" when their visibility is toggled
@@ -86,15 +80,12 @@ export default {
  * You can easily play with the modal transition by editing
  * these styles.
  */
-
 .modal-enter {
   opacity: 0;
 }
-
 .modal-leave-active {
   opacity: 0;
 }
-
 .modal-enter .modal-container,
 .modal-leave-active .modal-container {
   -webkit-transform: scale(1.1);
