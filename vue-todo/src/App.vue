8e42e7d4372c2307159c6648dcbe81d9014332bd
jyryuitpro <jy.ryu.itpro@gmail.com>
2022-03-07 10:13:35 +0900

    [리팩토링] 할 일 완료 기능

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -4,7 +4,7 @@
 <!-- <TodoInput v-on:하위 컴포넌트에서 발생시킨 이벤트 이름="현재 컴포넌트의 메서드 명"></TodoInput> -->
     <TodoInput v-on:addTodoItem="addOneItem"></TodoInput>
 <!-- <TodoList v-bind:내려보낼 프롭스 속성 이름="현재 위치의 컴포넌트 데이터 속성"></TodoList> -->
-    <TodoList v-bind:propsdata="todoItems" v-on:removeTodoItem="removeOneItem"></TodoList>
+    <TodoList v-bind:propsdata="todoItems" v-on:removeTodoItem="removeOneItem" v-on:toggleTodoItem="toggleOneItem"></TodoList>
     <TodoFooter></TodoFooter>
   </div>
 </template>
@@ -30,10 +30,17 @@ export default {
       localStorage.setItem(todoItem, JSON.stringify(obj));
       this.todoItems.push(obj);
     },
-    removeOneItem: function (todoItem, index ) {
+    removeOneItem: function (todoItem, index) {
       localStorage.removeItem(todoItem.item);
       // splice: 원본 수정 O, slice: 원본 수정 X
       this.todoItems.splice(index, 1);
+    },
+    toggleOneItem: function (todoItem, index) {
+      console.log(todoItem, index);
+      // todoItem.completed = !todoItem.completed;
+      this.todoItems[index].completed = !this.todoItems[index].completed;
+      localStorage.removeItem(todoItem.item);
+      localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
     }
   },
   created: function () {
--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -23,9 +23,7 @@ export default {
     },
     toggleComplete: function (todoItem, index) {
       console.log(todoItem, index);
-      todoItem.completed = !todoItem.completed;
-      localStorage.removeItem(todoItem.item);
-      localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
+      this.$emit('toggleTodoItem', todoItem, index);
     }
   }
 }
